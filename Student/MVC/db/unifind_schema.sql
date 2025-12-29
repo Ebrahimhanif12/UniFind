4e0446195f8b643c9f9357bc213069f62754d343
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-12-29 22:10:10 +0600

    feat: implement database schema for T-02

---
+++ Student/MVC/db/unifind_schema.sql
@@ -0,0 +1,61 @@
+
+
+-- ----------- users table--------------------
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    role ENUM('student', 'staff', 'admin') DEFAULT 'student',
+    karma_points INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ----------- items table--------------------
+CREATE TABLE items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL, -- Who posted the item
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    category VARCHAR(50), 
+    image_path VARCHAR(255),
+    status ENUM('lost', 'found', 'in_custody', 'returned') NOT NULL,
+    security_question VARCHAR(255), -- Only for Found items
+    security_answer VARCHAR(255),   -- Hashed answer for verification
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- ----------- claims table--------------------
+CREATE TABLE claims (
+    claim_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_id INT NOT NULL,
+    claimant_id INT NOT NULL, 
+    answer_attempt VARCHAR(255),
+    status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    claim_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE,
+    FOREIGN KEY (claimant_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- ----------- subscriptions table (for keyword subscription)--------------------
+CREATE TABLE subscriptions (
+    sub_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    keyword VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- ------------- custody table (For Admin tracking)----------------
+CREATE TABLE custody_logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_id INT NOT NULL,
+    admin_id INT NOT NULL,
+    action_type ENUM('check_in', 'hand_over', 'generate_qr'),
+    log_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
